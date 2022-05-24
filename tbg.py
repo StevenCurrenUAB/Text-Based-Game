fd4587b8281e3863a83305a750493f0661f347f9
Steven Curren <85853252+StevenCurrenUAB@users.noreply.github.com>
2022-05-24 11:24:44 -0500

    Start beginning

--- tbg.py
+++ tbg.py
@@ -1,4 +1,31 @@
 # Text Based Game for fun
 
-Inventory = []
+inventory = []
 
+def checkInventory():
+    if len(inventory) == 0:
+        print("The inventory is empty")
+    else:
+        for i in inventory:
+            print(i)
+
+while (True):
+
+    beginning = input("You wake up in your bedroom. What do you do?: ")
+
+    if beginning == "list commands":
+        print("look around, take chocolate, check inventory, sleep")
+
+    if beginning == "look around":
+        print("You look around your bedroom. There is a chocolate bar on the nightstand")
+
+    if beginning == "take chocolate":
+        print("You take the chocolate bar")
+        inventory.append("Chocolate Bar")
+
+    if beginning == "check inventory":
+        checkInventory()
+
+    if beginning == "sleep":
+        print("You go back to sleep")
+        break
